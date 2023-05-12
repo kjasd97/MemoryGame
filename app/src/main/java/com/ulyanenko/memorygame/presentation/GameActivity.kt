20133f32798f7c1ef33ce411e03c7e6ea55787e6
Roman Ulyanenko <ulyanenkorr@gmail.com>
2023-05-12 12:31:06 +0300

    add timer

--- app/src/main/java/com/ulyanenko/memorygame/presentation/GameActivity.kt
+++ app/src/main/java/com/ulyanenko/memorygame/presentation/GameActivity.kt
@@ -2,11 +2,12 @@ package com.ulyanenko.memorygame.presentation
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
 import android.widget.ImageButton
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.github.jinatonic.confetti.CommonConfetti
 import com.ulyanenko.memorygame.R
 import com.ulyanenko.memorygame.databinding.ActivityGameBinding
 
@@ -20,12 +21,16 @@ class GameActivity : AppCompatActivity() {
     private lateinit var buttons: List<ImageButton>
     private lateinit var cards: List<MemoryCard>
     private var indexOfSingleSelectedCard: Int? = null
+
     private var count: Int = 0
+    private var time = 0
     var numPairsFound = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        val startTime = System.currentTimeMillis()
 
         val images = mutableListOf(
             R.drawable.ic_heart,
@@ -38,7 +43,7 @@ class GameActivity : AppCompatActivity() {
         buttons = listOf(
             binding.imageButton1, binding.imageButton2, binding.imageButton3,
             binding.imageButton4, binding.imageButton5,
-            binding.imageButton6,
+            binding.imageButton6
         )
 
         cards = buttons.indices.map { index ->
@@ -46,15 +51,31 @@ class GameActivity : AppCompatActivity() {
         }
 
         binding.countOfAttempt.text = String.format(getString(R.string.attempt), returnCont())
+        binding.time.text = String.format(getString(R.string.time), time)
+
 
         buttons.forEachIndexed { index, button ->
             button.setOnClickListener {
+                it.animate().apply {
+                    duration = 1000
+                    rotationYBy(360f)
+                }.start()
+
                 count++
-                binding.countOfAttempt.text = String.format(getString(R.string.attempt), returnCont())
+                binding.countOfAttempt.text =
+                    String.format(getString(R.string.attempt), returnCont())
                 updateModels(index)
                 updateViews()
-                if(haveWonGame()){
+
+                if (haveWonGame()) {
+                    val endTime = System.currentTimeMillis()
+                    binding.time.text = String.format(getString(R.string.time), formatTime(endTime-startTime))
+
                     Toast.makeText(this, "You are winner", Toast.LENGTH_SHORT).show()
+                    CommonConfetti.rainingConfetti(
+                        binding.root,
+                        intArrayOf(Color.YELLOW, Color.GREEN, Color.MAGENTA)
+                    ).oneShot()
                 }
             }
         }
@@ -64,17 +85,9 @@ class GameActivity : AppCompatActivity() {
         }
 
         binding.restart.setOnClickListener {
-            images.shuffle()
-            cards = buttons.indices.map { index ->
-                MemoryCard(images[index])
-            }
-           buttons.forEach {
-               it.alpha = 1f
-               it.setImageResource(R.drawable.ic_code)
-           }
-            numPairsFound=0
-            count=0
-
+            val intent = intent
+            finish()
+            startActivity(intent)
         }
     }
 
@@ -118,8 +131,6 @@ class GameActivity : AppCompatActivity() {
         }
     }
 
-
-
     private fun checkForMatch(position1: Int, position2: Int) {
         if (cards[position1].identifier == cards[position2].identifier) {
             Toast.makeText(this, "Match found!!", Toast.LENGTH_SHORT).show()
@@ -133,14 +144,24 @@ class GameActivity : AppCompatActivity() {
         return numPairsFound == 3
     }
 
-    fun returnCont():Int{
-        return count/2
+    fun returnCont(): Int {
+        return count / 2
+    }
+
+    private fun formatTime(millis: Long): String {
+        val seconds = millis / MILLIS_IN_SECONDS
+        val minutes = seconds / SECONDS_IN_MINUTES
+        val leftSeconds = seconds - minutes * SECONDS_IN_MINUTES
+        return String.format("%02d:%02d", minutes, leftSeconds)
     }
 
     companion object {
         fun newIntent(context: Context): Intent {
             return Intent(context, GameActivity::class.java)
         }
-    }
 
+        private const val MILLIS_IN_SECONDS = 1000L
+        private const val SECONDS_IN_MINUTES = 60
+
+    }
 }
